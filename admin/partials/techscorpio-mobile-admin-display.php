18790af03c2af7db759acec5fba23156c979d2af
ch0nch0l <mehedi.chonchol@gmail.com>
2020-06-03 00:32:17 +0600

    Paid Content tab added to Admin Panel and PaidContentBySubscriptionPlan API endpoint added

--- admin/partials/techscorpio-mobile-admin-display.php
+++ admin/partials/techscorpio-mobile-admin-display.php
@@ -13,9 +13,17 @@
  */
 ?>
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div class="row">
+  <div class="col-10">
+    <h1 class="display-4 text-center">Techscorpio Mobile</h1>
+    <p class="lead text-center">This plugin helps your WordPress website to interact with TechScorpio Mobile App</p>
+  </div>
 
-<h1 class="display-4">Techscorpio Mobile</h1>
-<p class="lead">This plugin helps your WordPress website to interact with TechScorpio Mobile App</p>
+  <div class="col-2">
+    <img style="height:100px;" src="https://courticalhoops.com/wp-content/uploads/2020/06/Logo-Round.png"/>
+  </div>
+</div>
+
 
 <ul class="nav nav-tabs" id="myTab" role="tablist">
   <li class="nav-item">
@@ -25,17 +33,24 @@
     <a class="nav-link" id="free-content-tab" data-toggle="tab" href="#free-content" role="tab" aria-controls="free-content" aria-selected="false">Free Content</a>
   </li>
   <li class="nav-item">
+    <a class="nav-link" id="paid-content-tab" data-toggle="tab" href="#paid-content" role="tab" aria-controls="paid-content" aria-selected="false">Paid Content</a>
+  </li>
+  <li class="nav-item">
     <a class="nav-link" id="about-tab" data-toggle="tab" href="#about" role="tab" aria-controls="about" aria-selected="false">About</a>
   </li>
 </ul>
+
+
 <div class="tab-content" id="myTabContent">
+
+  <!--Home Slider-->
   <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
     <div><br>
       <form method="POST" action="<?php the_permalink() ?>">
         <div class="form-group row">								
           <label class="col-sm-2 col-form-label">Slider Image Url: </label>
           <div class="col-sm-8">
-            <input class="form-control" type="text" name="slider_url" placeholder="http://example.com/image.png"/>
+            <input required class="form-control" type="text" name="slider_url" placeholder="http://example.com/image.png"/>
           </div>
           <input class="btn btn-primary mb-2 pull-right" type="submit" name="insert_slider" value="Insert Slider Image" />
         </div>		        
@@ -72,27 +87,29 @@
 			</table>
   </div>
 
+  <!--Free Content-->
   <div class="tab-pane fade" id="free-content" role="tabpanel" aria-labelledby="free-content-tab">
-  <div><br>
+    <div>
+      <br>
       <form style="margin:10px;" method="POST" action="<?php the_permalink() ?>">
         <div class="form-group row">								
           <label class="col-sm-2 col-form-label">Free Content Url: </label>
           <div class="col-sm-10">
-            <input class="form-control" type="text" name="free_content_url" placeholder="http://example.com/video.mp4"/>
+            <input required class="form-control" type="text" name="free_content_url" placeholder="http://example.com/video.mp4"/>
           </div>
         </div>
         
         <div class="form-group row">
           <label class="col-sm-2 col-form-label">Content Title: </label>
           <div class="col-sm-10">
-            <input class="form-control" type="text" name="free_content_title" placeholder="Enter Title"/>
+            <input required class="form-control" type="text" name="free_content_title" placeholder="Enter Title"/>
           </div>
         </div>
         
         <div class="form-group row">
           <label class="col-sm-2 col-form-label">Content Description: </label>
           <div class="col-sm-10">
-            <textarea class="form-control" name="free_content_description" rows="3"></textarea>
+            <textarea required class="form-control" name="free_content_description" rows="3"></textarea>
           </div>        
         </div>
 
@@ -136,6 +153,92 @@
 
 			</table>
   </div>
+
+  <!--Paid Content-->
+  <div class="tab-pane fade" id="paid-content" role="tabpanel" aria-labelledby="paid-content-tab">
+    <div>
+      <br>
+      <form style="margin:10px;" method="POST" action="<?php the_permalink() ?>">
+      <div class="form-group row">								
+          <label class="col-sm-2 col-form-label">Paid Content Url: </label>
+          <div class="col-sm-10">
+            <?php 
+              $subs_plan_url = file_get_contents('https://courticalhoops.com/wp-json/ts_api/v1/get_subscription_plans');
+              $subs_plans = json_decode($subs_plan_url, TRUE);             
+            ?>
+            
+            <select class="col-sm-10" name="paid_content_subs_plan_name" id="paid_content_subs_plan_name">
+              <?php foreach($subs_plans as $subs_plan){ ?>
+                <option><?php echo $subs_plan['id'] ?> - <?php echo $subs_plan['name'] ?> </option>
+              <?php } ?>
+            </select>            
+          </div>
+        </div>
+
+        <div class="form-group row">								
+          <label class="col-sm-2 col-form-label">Paid Content Url: </label>
+          <div class="col-sm-10">
+            <input required class="form-control" type="text" name="paid_content_url" placeholder="http://example.com/video.mp4"/>
+          </div>
+        </div>
+        
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Content Title: </label>
+          <div class="col-sm-10">
+            <input required class="form-control" type="text" name="paid_content_title" placeholder="Enter Title"/>
+          </div>
+        </div>
+        
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Content Description: </label>
+          <div class="col-sm-10">
+            <textarea required class="form-control" name="paid_content_description" rows="3"></textarea>
+          </div>        
+        </div>
+
+        <input class="btn btn-primary mb-2 pull-right" type="submit" name="insert_paid_content" value="Insert Paid Content" />	     
+
+      </form>
+    </div>
+
+    <table class="table table-hover table-bordered">
+			  <tr>
+          <th class="text-center" style="width:5%;">SL.</th>
+          <th class="text-center" style="width:10%;">Subscription Plan</th>
+          <th class="text-center" style="width:10%;">Content View</th>
+          <th class="text-center" style="width:15%;">Content Title</th>
+          <th class="text-center" style="width:20%;">Content Description</th>
+          <th class="text-center" style="width:25%;">Content Url</th>
+          <th class="text-center">Action</th>
+        </tr>      
+        
+        <?php
+          global $wpdb;
+          $paid_content_table_name = '_ts_paid_content';
+          $existing_content_list = $wpdb->get_results( "SELECT * FROM $paid_content_table_name" );
+
+          
+          ?>
+
+          <?php foreach($existing_content_list as $existing_content){ ?>
+
+          <tr>
+            <td class="text-center align-middle"><?php echo $existing_content->paid_content_id; ?></td>
+            <td class="text-center align-middle"><?php echo $existing_content->paid_content_subs_plan_name; ?></td>
+            <td class="text-center"><img style="height:100px;" src="https://img.youtube.com/vi/<?php echo getYouTubeIdFromURL($existing_content->paid_content_url); ?>/default.jpg"/></td>
+            <td class="text-center align-middle"><?php echo $existing_content->paid_content_title; ?></td>
+            <td class="text-center align-middle"><?php echo $existing_content->paid_content_description; ?></td>
+            <td class="align-middle"><?php echo $existing_content->paid_content_url; ?></td>
+            <td class="text-center align-middle">
+              <button name="edit_slider" type="submit" class="btn btn-sm btn-info">Edit</button>
+              <button name="delete_slider" type="button" class="btn btn-sm btn-danger">Delete</button>
+            </td>
+          </tr>
+        <?php } ?>
+
+			</table>
+  </div>
+
   <div class="tab-pane fade" id="about" role="tabpanel" aria-labelledby="about-tab">
     
   </div>
@@ -148,6 +251,8 @@ if(isset($_POST['insert_slider'])){
   insert_slider_data();
 } else if(isset($_POST['insert_free_content'])){
   insert_free_content_data();
+} else if(isset($_POST['insert_paid_content'])){
+  insert_paid_content_data();
 }
 
 function insert_slider_data(){	
@@ -198,6 +303,35 @@ function insert_free_content_data(){
 	$item = shortcode_atts( $default, $_REQUEST );
 
 	$wpdb->insert( $free_content_table_name, $item );
+	
+	echo "<script type='text/javascript'>
+        window.location=document.location.href;
+        </script>";
+}
+
+function insert_paid_content_data(){	
+	global $wpdb;
+	$paid_content_table_name = '_ts_paid_content';
+	
+	$default_row = $wpdb->get_row( "SELECT * FROM $paid_content_table_name ORDER BY paid_content_id DESC LIMIT 1" );
+  
+	if ( $default_row != null ) {
+		$id = $default_row->paid_content_id + 1;
+	} else {
+		$id = 1;
+	}
+
+	$default = array(
+		'paid_content_id' => $id,
+		'paid_content_subs_plan_name' => $_POST['paid_content_subs_plan_name'],
+		'paid_content_url' => $_POST['paid_content_url'],
+		'paid_content_title' => $_POST['paid_content_title'],
+		'paid_content_description' => $_POST['paid_content_description'],
+	);
+
+	$item = shortcode_atts( $default, $_REQUEST );
+
+	$wpdb->insert( $paid_content_table_name, $item );
 	
 	echo "<script type='text/javascript'>
         window.location=document.location.href;
--- public/rest-api/techscorpio-mobile-controller.php
+++ public/rest-api/techscorpio-mobile-controller.php
@@ -6,7 +6,9 @@ class TechScorpio_Mobile_Controller extends WP_REST_Controller {
 		$namespace = 'ts_api/v1';
 		$slider_image_path = 'get_slider_image';
 		$free_content_path = 'get_free_content';
-		$profile_image_path = 'get_profile_image/(?P<user_id>\\d+)';
+		$paid_content_path = 'get_paid_content';
+		$paid_content_by_subs_plan_path = 'get_paid_content_by_subs_plan/(?P<plan_id>[\d]+)';
+		$profile_image_path = 'get_profile_image/(?P<user_id>[\d]+)';
 		$subscription_plans = 'get_subscription_plans';
 
 		$register_user_path = 'register_new_user';
@@ -26,6 +28,25 @@ class TechScorpio_Mobile_Controller extends WP_REST_Controller {
 			array(
 			'methods'             => 'GET',
 			'callback'            => array( $this, 'get_free_content' ),
+			'permission_callback' => array( $this, 'get_items_permissions_check' )
+			),
+	
+		]);
+
+		//Paid Content
+		register_rest_route( $namespace, '/' . $paid_content_path , [
+			array(
+			'methods'             => 'GET',
+			'callback'            => array( $this, 'get_paid_content' ),
+			'permission_callback' => array( $this, 'get_items_permissions_check' )
+			),
+	
+		]);
+
+		register_rest_route( $namespace, '/' . $paid_content_by_subs_plan_path , [
+			array(
+			'methods'             => 'GET',
+			'callback'            => array( $this, 'get_paid_content_by_subs_plan' ),
 			'permission_callback' => array( $this, 'get_items_permissions_check' )
 			),
 	
@@ -94,14 +115,53 @@ class TechScorpio_Mobile_Controller extends WP_REST_Controller {
 		
 		return new WP_REST_Response($free_content, 200);
 	}
+
+	public function get_paid_content($request) {
+		
+		global $wpdb;
+		$table_name = '_ts_paid_content';
+		
+		$free_content = $wpdb->get_results( "SELECT * FROM $table_name" );	
+
+		if (empty($free_content)) {
+
+			return new WP_Error( 'empty_paid_content', 'there is no paid_content in the database', array( 'status' => 404 ) );
+		}
+		
+		return new WP_REST_Response($free_content, 200);
+	}
+
+	public function get_paid_content_by_subs_plan($request) {
+		
+		global $wpdb;
+		$table_name = '_ts_paid_content';
+		$subs_table_name = $wpdb->prefix.'pms_member_subscriptions';
+		//$subs_id = $request['plan_id'];
+
+		$free_content = $wpdb->get_results( 
+			"SELECT $table_name.paid_content_id,
+			$table_name.paid_content_subs_plan_name,
+			$table_name.paid_content_url,
+			$table_name.paid_content_title,
+			$table_name.paid_content_description
+			FROM $table_name 
+			INNER JOIN $subs_table_name 
+			ON LEFT($table_name.paid_content_subs_plan_name, LOCATE(' ',$table_name.paid_content_subs_plan_name) - 1) 
+			= $subs_table_name.subscription_plan_id" );
+
+		if (empty($free_content)) {
+
+			return new WP_Error( 'empty_free_content', 'there is no free_content in the database', array( 'status' => 404 ) );
+		}
+		
+		return new WP_REST_Response($free_content, 200);
+	}
 	
-	public function get_profile_image($request) {				
+	public function get_profile_image($request) {
 
-		$args = array(
-            'user_id' => $request['user_id']
-    	);
+        $user_id = $request['user_id'];    	
 
-    	$profile_image = get_avatar_url($args);
+    	$profile_image = get_avatar_url($user_id);
 
 		if (empty($profile_image)) {
 
@@ -120,6 +180,12 @@ class TechScorpio_Mobile_Controller extends WP_REST_Controller {
 		
 		return new WP_REST_Response($subscription_plans, 200);
 	}
+
+	function get_subs_id_from_param($url) {
+		$url_string = parse_url($url, PHP_URL_QUERY);
+		parse_str($url_string, $args);
+		return isset($args['v']) ? $args['v'] : false;
+	  }
 	
 } 
 
